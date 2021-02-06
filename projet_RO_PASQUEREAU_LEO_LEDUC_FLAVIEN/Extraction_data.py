d8d190deee2dd26968bf1d144ef3363bb7541014
FlavLdc <57667777+FlavLdc@users.noreply.github.com>
2021-02-06 18:14:47 +0100

    modifs nodes

--- projet_RO_PASQUEREAU_LEO_LEDUC_FLAVIEN/Extraction_data.py
+++ projet_RO_PASQUEREAU_LEO_LEDUC_FLAVIEN/Extraction_data.py
@@ -25,17 +25,14 @@ def extract_data(file_to_open):
                 a = line_split[0]
                 b = line_split[1]
                 c = line_split[2]
-                graph.add_nodes_from([(a,{"demande" :c})])
-                
+                graph.add_node(a,demande = c)     
             elif bloc =='ROADS':
                 d = line_split[0]
                 e = line_split[1]
                 f = line_split[2]
                 g = line_split[3]
                 h = line_split[4]
-
-                #graph.add_node(e)
-                graph.add_edge(d,e,capacity=f, length =g)
+                graph.add_edge(d,e,capacity=f, Essence =g, Douane =h)
             else:
                 exit(f'ERROR: line = {line}')
         if(n_clientsuppr == 1):
