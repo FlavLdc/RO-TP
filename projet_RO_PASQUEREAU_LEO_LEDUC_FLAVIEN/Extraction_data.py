99bfb91f9b32c4bd3df0e72c97c2005455439f34
Lpasquer <71018559+Lpasquer@users.noreply.github.com>
2021-02-07 18:52:58 +0100

    changes

--- projet_RO_PASQUEREAU_LEO_LEDUC_FLAVIEN/Extraction_data.py
+++ projet_RO_PASQUEREAU_LEO_LEDUC_FLAVIEN/Extraction_data.py
@@ -9,10 +9,10 @@ def extract_data(file_to_open):
     with open(file_to_open,'r') as f_in:
         #Extraction des données de l'en-tête
         entete = f_in.readline()
-        p = entete.split()[0] # Limite du camion
+        p = int(entete.split()[0]) # Limite du camion
         start =entete.split()[1] #Depot de départ
-        n_clientsuppr = entete.split()[2]#Nb de clients à ne pas servir
-        n_depsuppr =entete.split()[3]# Nb de depots HS
+        n_clientsuppr = int(entete.split()[2])#Nb de clients à ne pas servir
+        n_depsuppr = int(entete.split()[3])# Nb de depots HS
         #Extraction des données des blocs
         bloc = ''
         for line in f_in:
@@ -24,24 +24,39 @@ def extract_data(file_to_open):
             elif bloc == 'ENTITIES':
                 a = line_split[0]
                 b = line_split[1]
-                c = line_split[2]
-                graph.add_node(a,demande = c)     
+                c = int(line_split[2])
+                if(b == 'depot'):
+                    graph.add_node(a, Stock = c)
+                else:                   
+                    graph.add_node(a, Demande = c)     
             elif bloc =='ROADS':
                 d = line_split[0]
                 e = line_split[1]
-                f = line_split[2]
-                g = line_split[3]
-                h = line_split[4]
-                graph.add_edge(d,e,capacity=f, Essence =g, Douane =h)
+                f = int(line_split[2])
+                g = int(line_split[3])
+                h = int(line_split[4])
+                graph.add_edge(d, e, Capacity=f, Essence=g, Douane=h)
             else:
                 exit(f'ERROR: line = {line}')
         if(n_clientsuppr == 1):
             rdm = random.choice(['C1','C2'])
             graph.remove_node(rdm)
-    return graph
+        elif(n_clientsuppr == 2):
+            graph.remove_node('C1')
+            graph.remove_node('C2')
+        if(n_depsuppr == 1):
+            rdm = random.choice(['D1','D2','D3'])
+            graph.remove_node(rdm)
+        elif(n_depsuppr == 2):
+            depot = ['D1','D2','D3']
+            rdm = random.choice(depot)
+            depot.remove(rdm)
+            rdm2 = random.choice(depot)
+            graph.remove_node(rdm)          
+            graph.remove_node(rdm2)
+        elif(n_depsuppr == 3):
+            graph.remove_node('D1')
+            graph.remove_node('D2')
+            graph.remove_node('D3')
+    return graph, start, p
 
-
-if __name__ == '__main__':
-    a = random.choice(['a','b'])
-
-    print(a)
--- projet_RO_PASQUEREAU_LEO_LEDUC_FLAVIEN/truck_pulp.py
+++ projet_RO_PASQUEREAU_LEO_LEDUC_FLAVIEN/truck_pulp.py
@@ -7,55 +7,96 @@ import pulp as pl
 import networkx as nx
 from Extraction_data import extract_data
 
-data_folder = Path("./data")
-
-file_to_open = data_folder/"truck_instance_less_customers.data"
-
-f = open(file_to_open)
 
 # ============================================================================ #
 #                                  SET MODEL                                   #
 # ============================================================================ #
-def set_model_name():
-    """TODO: Description."""
+def set_benef_max(graph, source, p):
+    """Set the max benef possible from a graph"""
     # ------------------------------------------------------------------------ #
     # Linear problem with maximization
     # ------------------------------------------------------------------------ #
-    prob = pl.LpProblem(name='name', sense=pl.LpMaximize)
-    # FIXME: it is not always a maximization problem ...
+    prob = pl.LpProblem(name='Maximisation_du_benefice', sense=pl.LpMaximize)
 
     # ------------------------------------------------------------------------ #
     # The variables
     # ------------------------------------------------------------------------ #
-    # TODO: set variables
+    Points = graph.nodes() #dictionnaire ['D1','D2',...]
+    Routes = graph.edges() #dictionnaire [('D1','D2'),('D1','D3'),...]
+    Depart = source
+    Arrivee = source
+    p_camion = p
+
+    Essence = nx.get_edge_attributes(graph,'Essence') #list {('D1', 'D2'): 20, ('D1', 'D3'): 20,...}
+    Capacity = nx.get_edge_attributes(graph, 'Capacity') #list {('D1', 'D2'): 10, ('D1', 'D3'): 10,...}
+    Douane = nx.get_edge_attributes(graph, 'Douane') #list {('D1', 'D2'): 30, ('D1', 'D3'): 30,...}
+    Demande_clients = nx.get_node_attributes(graph, 'Demande') #list {'C1': 8, 'C2': 10}
+    Stock_depots = nx.get_node_attributes(graph, 'Stock') #list {'D1': -10, 'D2': -3, 'D3': -5}
+    
+    Camion = pl.LpVariable.dicts('Gpu_Camion', Routes, lowBound=0, cat=pl.LpInteger)
+    Livraison = pl.LpVariable.dicts('Livraison', Points, lowBound=0, cat=pl.LpInteger)
+    Quantite_depot = pl.LpVariable.dicts('Quantite', Points, upBound=0, cat=pl.LpInteger)
+
+    #big_M = graph.number_of_edges()
+
+    d_passage_e = {}  # a dictionnary of LpVariable which means: is e in path?
+    for (u,v) in Routes:
+        # Add the edges binaries
+        d_passage_e[(u, v)] = pl.LpVariable(f'passage_{u}_{v}', cat=pl.LpBinary)
 
     # ------------------------------------------------------------------------ #
     # The objective function
     # ------------------------------------------------------------------------ #
     # TODO: write the objective function
-
+    prob += pl.lpSum([1000 * pl.lpSum(Demande_clients)]) - pl.lpSum([(Essence[e] + Douane[e] * Camion[e]) for e in Routes])
     # ------------------------------------------------------------------------ #
     # The constraints
     # ------------------------------------------------------------------------ #
     # TODO: write constraints
+    # C1: capacity is the transport limit for each edge
+    for e in Routes:
+        prob += Camion[e] <= Routes[e]['Capacity'], f'capacity_limit_{e}'
+        prob += Camion[e] <= p_camion
 
+    #C2 : On doit commencer a la source et finir a la source
+    
+    # On ne peut passer qu'1 fois sur un chemin 
+    for point in Points:
+        if point == source:
+            prob += pl.lpSum(d_passage_e[(u,v)] for u, v in Routes if u == point) == 1
+            prob += pl.lpSum(d_passage_e[(t,u)] for u, t in Routes if u == point) == 1
+        #else:
+            #prob += pl.lpSum(Transport_Camion[u,v] for v, u in Routes if v == point) - \
+             #       pl.lpSum(Transport_Camion[v,w] for w, v in Routes if v == point) == Demande_clients[point]
+    print(Stock_depots)
+    
+    for e in Routes:
+        prob += d_passage_e[e] <= 1, f'{e}_max_one_time_in_path'
+        
+        for (u,v) in e:
+            if u == source:
+                prob += pl.lpSum(d_passage_e[(u,v)] for u, v in Routes) == 1
+            elif v == source:
+                prob += pl.lpSum(d_passage_e[(u,v)] for u, v in Routes) == 1
+            
     return prob
 
 # ============================================================================ #
 #                               SOLVE WITH DATA                                #
 # ============================================================================ #
 
-def solve_something():
+def solve_truck_problem(file_path):
     """TODO: Description."""
     # ------------------------------------------------------------------------ #
     # Set data
     # ------------------------------------------------------------------------ #
     # TODO: set data
+    graph, source, p = extract_data(file_path)
 
     # ------------------------------------------------------------------------ #
     # Solve the problem using the model
     # ------------------------------------------------------------------------ #
-    prob = set_model_name()
+    prob = set_benef_max(graph,source,p)
     # Coin Branch and Cut solver is used to solve the instanced model
     # TODO: change the log path file
     prob.solve(pl.PULP_CBC_CMD(logPath='./log_path_file.log'))
@@ -73,7 +114,16 @@ def solve_something():
     for v in prob.variables():
         print(v.name, '=', v.varValue)
 
+    print("benefice max : ", pl.value(prob.objective) )
+
 
 if __name__ == '__main__':
-    graph =extract_data(file_to_open)
-    nx.write_graphml(graph,'test_clientsuppr.graphml')
+
+    data_folder = Path("./data")
+
+    file_to_open = data_folder/"truck_instance_base.data"
+
+    graph = extract_data(file_to_open)
+    solve_truck_problem(file_to_open)
+    #print(nx.get_node_attributes(graph,'Demande'))
+    #nx.write_graphml(graph,'test_clientsuppr.graphml')
